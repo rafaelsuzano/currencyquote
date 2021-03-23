c2958d7406ecbdcb675853352b9202facf771b84
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-03-23 15:07:00 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('TESTE !!! ');
+  res.end('TESTE TESTE');
 });
 
 server.listen(port, hostname, () => {
